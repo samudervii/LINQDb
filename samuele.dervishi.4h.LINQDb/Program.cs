afff29788e925d244275a8598e6be35e13f48c49
Samuele Dervishi <117899052+samudervii@users.noreply.github.com>
2023-05-10 21:22:42 +0200

    Add files via upload

---
+++ samuele.dervishi.4h.LINQDb/Program.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+
+//Connessione al Database
+SQLiteConnection cn1 = new SQLiteConnection("chinook.db");
+
+//Richieste al Database
+var tblArtists = cn1.Query<Artist>("select * from artists");
+
+//Stampa su console di quanti record sono presenti in tblArtists
+Console.WriteLine($"In questa tabella ci sono {tblArtists.Count} record!");
+
+//Language Integrate Query
+//LINQ
+
+tblArtists = tblArtists.OrderByDescending(x=>x.Name).ToList();
+foreach(var artista in tblArtists)
+{
+    Console.WriteLine($"{artista.Name}");
+}
+
+
+
+//Classe di contenimento dei dati
+public class Artist
+{
+    public int ArtistId{get;set;}
+    public string Name{get;set;}
+}
